be42eb6d7b1bd5b29a600d17607f90e89bbb72ea
lloyd aleister bonds <lloydabonds@gmail.com>
2026-01-02 21:34:36 -0500

    jwt service is about 90% done

--- backend/ragaas/src/main/java/infera/backend/ragaas/services/JwtService.java
+++ backend/ragaas/src/main/java/infera/backend/ragaas/services/JwtService.java
@@ -3,18 +3,53 @@
 // It is not connected to any controllers.
 package infera.backend.ragaas.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+
 import java.util.Map;
+
+import javax.crypto.SecretKey;
+
+import java.util.Date;
 
 @Service
 public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private String jwtExpirationTimeString;
+
+    private long jwtExpirationTime = Long.parseLong(jwtExpirationTimeString);
+    private SecretKey key;
+
+    @PostConstruct
+    public void init() {
+        if (jwtSecret == null || jwtSecret.length() < 32) {
+            throw new IllegalStateException("jwt secret is invalid, not present, or too short");
+        }
+        this.key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
+
     public String generateJwtToken(Map<String, Object> userDetails) {
-        System.out.println(userDetails);
-        return "yo";
+        Date now = new Date();
+        Date expireDate = new Date(now.getTime() + jwtExpirationTime);
+        return Jwts.builder()
+                .claims(userDetails)
+                .signWith(key)
+                .issuedAt(now)
+                .expiration(expireDate)
+                .compact();
+
     }
 
     public long getExpirationTime() {
-        return 83766690;
+        return jwtExpirationTime;
     }
 
 }
