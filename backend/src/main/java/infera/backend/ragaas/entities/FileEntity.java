8783b45de59ab523b6b952ba36603edde56a0150
lloyd aleister bonds <lloydabonds@gmail.com>
2026-01-05 18:29:00 -0500

    added fileentity

--- backend/src/main/java/infera/backend/ragaas/entities/FileEntity.java
+++ backend/src/main/java/infera/backend/ragaas/entities/FileEntity.java
@@ -1,8 +1,98 @@
-import jakarta.persistence.Entity;
-import jakarta.persistence.Index;
-import jakarta.persistence.Table;
+package infera.backend.ragaas.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "files", indexes = {
-@Index(name = "idx_filename", columnList = "filename")
+        @Index(name = "idx_id", columnList = "id"),
+        @Index(name = "idx_userId", columnList = "userId")
 })
+public class FileEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private long userId;
+
+    @Column(nullable = false)
+    private String filename;
+
+    @Column(nullable = false)
+    private long fileSize;
+
+    @Column(nullable = false)
+    private String contentType;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(name = "uploadedAt", nullable = false, updatable = false)
+    private LocalDateTime uploadedAt;
+
+    @Column(nullable = false, unique = true)
+    private String filePath;
+
+    @PrePersist
+    protected void onCreate() {
+        uploadedAt = LocalDateTime.now();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void getStatus(String status) {
+        this.status = status;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
