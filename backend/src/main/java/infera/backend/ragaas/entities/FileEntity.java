ec82f918c30acba6788a7ab661426f35fe9955e9
lloyd aleister bonds <lloydabonds@gmail.com>
2026-01-05 18:03:20 -0500

    making file entity

--- backend/src/main/java/infera/backend/ragaas/entities/FileEntity.java
+++ backend/src/main/java/infera/backend/ragaas/entities/FileEntity.java
@@ -1,7 +1,8 @@
 import jakarta.persistence.Entity;
+import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "files", indexes = {
-
+@Index(name = "idx_filename", columnList = "filename")
 })
